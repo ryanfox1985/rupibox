3a6fb09a899511ecead6f2e79388c409da4b49cc
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2015-09-13 23:45:04 +0200

    Updated seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Pin.create({name: 'Plug 0', pin_pi: 17, value: false})
-Pin.create({name: 'Plug 1', pin_pi: 27, value: false})
-Pin.create({name: 'Plug 2', pin_pi: 22, value: false})
-Pin.create({name: 'Plug 3', pin_pi: 23, value: false})
+pin1 = Pin.find_or_create_by(pin_pi: 17)
+pin1.attributes = {name: 'Plug 0', pin_pi: 17, value: false}
+pin1.save
+
+pin2 = Pin.find_or_create_by(pin_pi: 27)
+pin2.attributes = {name: 'Plug 1', pin_pi: 27, value: false}
+pin2.save
+
+pin3 = Pin.find_or_create_by(pin_pi: 22)
+pin3.attributes = {name: 'Plug 2', pin_pi: 22, value: false}
+pin3.save
+
+pin4 = Pin.find_or_create_by(pin_pi: 23)
+pin4.attributes = {name: 'Plug 3', pin_pi: 23, value: false}
+pin4.save
