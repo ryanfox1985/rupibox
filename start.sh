0bb6ebe5e3c9b64997fd8d5c92ed5b5510cf0c67
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2015-09-14 00:16:46 +0200

    Refactors.

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 /bin/bash -l -c "bundle install --without development test profile --deployment"
 /bin/bash -l -c "bundle exec rake db:create"
 /bin/bash -l -c "bundle exec rake assets:precompile db:migrate"
+/bin/bash -l -c "bundle exec rake db:seed"
 /bin/bash -l -c "bundle exec puma -C config/puma.rb"
