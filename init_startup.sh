878efdadcf385f6f9b393944c3aedcb2a2d8bd31
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2015-09-27 17:20:04 +0200

    Updated init_script.

--- init_startup.sh
+++ init_startup.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f /root/rupibox/docker-compose.yml up
+# cp init_startup.sh /etc/init.d/init_startup.sh
+# update-rc.d init_startup.sh defaults
+
+do_start () {
+	/usr/local/bin/docker-compose -f /root/rupibox/docker-compose.yml up -d
+}
+
+do_stop () {
+	/usr/local/bin/docker-compose -f /root/rupibox/docker-compose.yml stop
+}
+
+case "$1" in
+  start|"")
+	do_start
+	;;
+  restart|reload|force-reload)
+	do_stop
+	;;
+  stop)
+	do_stop
+	;;
+  *)
+	echo "Usage: init_startup.sh [start|restart|stop]" >&2
+	exit 3
+	;;
+esac
